488cc1451a5ea9958846dc6d8df4d55026fa818d
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-02-18 09:39:36 -0500

    Normalize to StandardJS

--- bin/postman-to-k6.js
+++ bin/postman-to-k6.js
@@ -1,56 +1,54 @@
 #!/usr/bin/env node
-var program = require('commander'),
-    fs = require('fs'),
-    stripJSONComments = require('strip-json-comments');
+var program = require('commander')
+
+var fs = require('fs')
+
+var stripJSONComments = require('strip-json-comments')
 
 // describe the options and usage instruction for the `convert` command
 program
-    .version(require('../package.json').version)
-    .usage('<filePath> [options]')
-    .description('Convert a Postman collection to Load Impact Lua user scenario')
-    .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2.0.0|1.0.0)$/i, '2.0.0')
-    .option('-o --output <path>', 'Target file path where the converted collection will be written. Default `console`')
-    .action(function (fileName, options) {
+  .version(require('../package.json').version)
+  .usage('<filePath> [options]')
+  .description('Convert a Postman collection to Load Impact Lua user scenario')
+  .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2.0.0|1.0.0)$/i, '2.0.0')
+  .option('-o --output <path>', 'Target file path where the converted collection will be written. Default `console`')
+  .action(function (fileName, options) {
+    var input
+    try {
+      input = loadJSON(fileName)
+    } catch (e) {
+      console.error('unable to load the input file!', e)
+      return
+    }
 
-      var input;
-      try {
-        input = loadJSON(fileName);
-      } catch (e) {
-        console.error('unable to load the input file!', e);
-        return;
+    var converter = require('../lib/converters/postman-' + options.inputVersion)
+    if (!converter) {
+      console.error('unable to load converter ' + options.inputVersion)
+      return
+    }
+
+    converter.convert(input, function (error, result) {
+      if (error) {
+        console.error(error)
+        return
       }
 
-      var converter = require('../lib/converters/postman-' + options.inputVersion);
-      if (!converter) {
-        console.error('unable to load converter ' + options.inputVersion);
-        return;
+      if (options.output) {
+        fs.writeFile(options.output, result, function (error) {
+          if (error) {
+            console.error('Count not create output ' + options.output)
+            console.error(error)
+          }
+        })
+      } else {
+        console.log(result)
       }
+    })
+  })
 
-      converter.convert(input, function(error, result) {
-        if (error) {
-          console.error(error);
-          return;
-        }
+program.parse(process.argv)
 
-        if (options.output) {
-          fs.writeFile(options.output, result, function(error) {
-            if (error) {
-              console.error('Count not create output '+options.output);
-              console.error(error);
-            }
-          });
-        } else {
-          console.log(result);
-        }
-      });
-
-
-    });
-
-
-program.parse(process.argv);
-
-function loadJSON(path) {
-  var data = fs.readFileSync(path);
-  return JSON.parse(stripJSONComments(data.toString()));
+function loadJSON (path) {
+  var data = fs.readFileSync(path)
+  return JSON.parse(stripJSONComments(data.toString()))
 };
