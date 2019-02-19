7943c3ccf11df042e3c8da0ad69838b683ad6d4e
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-02-19 10:44:10 -0500

    Correct version matching

--- bin/postman-to-k6.js
+++ bin/postman-to-k6.js
@@ -10,7 +10,7 @@ program
   .version(require('../package.json').version)
   .usage('<filePath> [options]')
   .description('Convert a Postman collection to Load Impact Lua user scenario')
-  .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2.0.0|1.0.0)$/i, '2.0.0')
+  .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2\.0\.0|1\.0\.0)$/i, '2.0.0')
   .option('-o --output <path>', 'Target file path where the converted collection will be written. Default `console`')
   .action(function (fileName, options) {
     var input
