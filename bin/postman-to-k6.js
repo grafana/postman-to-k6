42ae2bf358df5a2dbcf5495fda8c4f4e1c729431
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-02-19 10:51:04 -0500

    Correct CLI program description

--- bin/postman-to-k6.js
+++ bin/postman-to-k6.js
@@ -9,7 +9,7 @@ var convertObject = require('../lib/convert/object')
 program
   .version(require('../package.json').version)
   .usage('<filePath> [options]')
-  .description('Convert a Postman collection to Load Impact Lua user scenario')
+  .description('Convert a Postman collection to k6 script')
   .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2\.0\.0|1\.0\.0)$/, '2.0.0')
   .option('-o --output <path>', 'Target file path where the converted collection will be written. Default `console`')
   .action(function (fileName, options) {
