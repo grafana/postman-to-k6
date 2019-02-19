43f37abb62786093b7ef506b7763b2f28a6ae686
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-02-19 11:30:49 -0500

    bin: Factor run procedure

--- bin/postman-to-k6.js
+++ bin/postman-to-k6.js
@@ -11,25 +11,27 @@ program
   .description('Convert a Postman collection to k6 script')
   .option('-j --input-version <version>', 'Input version. Options `2.0.0` or `1.0.0`. Default `2.0.0`.', /^(2\.0\.0|1\.0\.0)$/, '2.0.0')
   .option('-o --output <path>', 'Target file path where the converted collection will be written. Default `console`')
-  .action(function (path, options) {
-    const version = options.inputVersion
-    convertFile(path, version, (error, result) => {
-      if (error) {
-        console.error(error.message)
-        return
-      }
-
-      if (options.output) {
-        fs.writeFile(options.output, result, function (error) {
-          if (error) {
-            console.error('could not create output ' + options.output)
-            console.error(error)
-          }
-        })
-      } else {
-        console.log(result)
-      }
-    })
-  })
+  .action(run)
 
 program.parse(process.argv)
+
+function run (path, options) {
+  const version = options.inputVersion
+  convertFile(path, version, (error, result) => {
+    if (error) {
+      console.error(error.message)
+      return
+    }
+
+    if (options.output) {
+      fs.writeFile(options.output, result, function (error) {
+        if (error) {
+          console.error('could not create output ' + options.output)
+          console.error(error)
+        }
+      })
+    } else {
+      console.log(result)
+    }
+  })
+}
