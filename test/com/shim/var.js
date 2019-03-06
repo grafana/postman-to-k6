cc0b483773f33da84278d17f9e645d5b2058909c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-03-06 18:54:58 -0500

    shim: Test environment variables

--- test/com/shim/var.js
+++ test/com/shim/var.js
@@ -1,4 +1,4 @@
-/* global postman globals */
+/* global postman globals environment */
 
 import test from 'ava'
 import 'shim'
@@ -17,14 +17,14 @@ test.serial('globals read clear', t => {
 })
 
 test.serial('globals read set', t => {
-  postman[Scope]({ globals: { test: 6 } })
-  t.is(globals.test, 6)
+  postman[Scope]({ globals: { test: 'a' } })
+  t.is(globals.test, 'a')
 })
 
 test.serial('globals write', t => {
   postman[Scope]()
   t.throws(() => {
-    globals.test = 8
+    globals.test = 'a'
   })
 })
 
@@ -34,34 +34,89 @@ test.serial('getGlobalVariable clear', t => {
 })
 
 test.serial('getGlobalVariable set', t => {
-  postman[Scope]({ globals: { test: 8 } })
-  t.is(postman.getGlobalVariable('test'), 8)
+  postman[Scope]({ globals: { test: 'a' } })
+  t.is(postman.getGlobalVariable('test'), 'a')
 })
 
 test.serial('setGlobalVariable clear', t => {
   postman[Scope]()
-  postman.setGlobalVariable('test', 8)
-  t.is(globals.test, 8)
+  postman.setGlobalVariable('test', 'a')
+  t.is(globals.test, 'a')
 })
 
 test.serial('setGlobalVariable set', t => {
-  postman[Scope]({ globals: { test: 8 } })
-  postman.setGlobalVariable('test', 10)
-  t.is(globals.test, 10)
+  postman[Scope]({ globals: { test: 'a' } })
+  postman.setGlobalVariable('test', 'b')
+  t.is(globals.test, 'b')
 })
 
 test.serial('clearGlobalVariable', t => {
-  postman[Scope]({ globals: { test: 8 } })
-  t.is(globals.test, 8)
+  postman[Scope]({ globals: { test: 'a' } })
+  t.is(globals.test, 'a')
   postman.clearGlobalVariable('test')
   t.is(globals.test, undef)
 })
 
 test.serial('clearGlobalVariables', t => {
-  postman[Scope]({ globals: { test: 8, test2: 10 } })
-  t.is(globals.test, 8)
-  t.is(globals.test2, 10)
+  postman[Scope]({ globals: { test: 'a', test2: 'b' } })
+  t.is(globals.test, 'a')
+  t.is(globals.test2, 'b')
   postman.clearGlobalVariables()
   t.is(globals.test, undef)
   t.is(globals.test2, undef)
+})
+
+test.serial('environment read clear', t => {
+  postman[Scope]()
+  t.is(environment.test, undef)
+})
+
+test.serial('environment read set', t => {
+  postman[Scope]({ environment: { test: 'a' } })
+  t.is(environment.test, 'a')
+})
+
+test.serial('environment write', t => {
+  postman[Scope]()
+  t.throws(() => {
+    environment.test = 'a'
+  })
+})
+
+test.serial('getEnvironmentVariable clear', t => {
+  postman[Scope]({ environment: {} })
+  t.is(postman.getEnvironmentVariable('test'), undef)
+})
+
+test.serial('getEnvironmentVariable set', t => {
+  postman[Scope]({ environment: { test: 'a' } })
+  t.is(postman.getEnvironmentVariable('test'), 'a')
+})
+
+test.serial('setEnvironmentVariable clear', t => {
+  postman[Scope]({ environment: {} })
+  postman.setEnvironmentVariable('test', 'a')
+  t.is(environment.test, 'a')
+})
+
+test.serial('setEnvironmentVariable set', t => {
+  postman[Scope]({ environment: { test: 'a' } })
+  postman.setEnvironmentVariable('test', 'b')
+  t.is(environment.test, 'b')
+})
+
+test.serial('clearEnvironmentVariable', t => {
+  postman[Scope]({ environment: { test: 'a' } })
+  t.is(environment.test, 'a')
+  postman.clearEnvironmentVariable('test')
+  t.is(environment.test, undef)
+})
+
+test.serial('clearEnvironmentVariables', t => {
+  postman[Scope]({ environment: { test: 'a', test2: 'b' } })
+  t.is(environment.test, 'a')
+  t.is(environment.test2, 'b')
+  postman.clearEnvironmentVariables()
+  t.is(environment.test, undef)
+  t.is(environment.test2, undef)
 })
