7d75b2707917260ce78a93bfe6190a0e47007696
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-03-16 22:04:51 -0400

    Error on responseCode.name

--- test/com/shim/response.js
+++ test/com/shim/response.js
@@ -37,6 +37,16 @@ test.serial('responseCode.code', t => {
   })
 })
 
+test.serial('responseCode.name', t => {
+  postman[Request]({
+    post () {
+      t.throws(() => {
+        responseCode.name /* eslint-disable-line no-unused-expressions */
+      })
+    }
+  })
+})
+
 test.serial('responseTime', t => {
   http.request.returns({ timings: { duration: 556 } })
   postman[Request]({
