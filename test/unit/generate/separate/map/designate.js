1e984bb124e7a99b57f137cd81e4d5fda2939840
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-12 01:15:04 -0400

    separate: Update filename encoding tests

--- test/unit/generate/separate/map/designate.js
+++ test/unit/generate/separate/map/designate.js
@@ -2,7 +2,6 @@ import test from 'ava'
 import mockRequire from 'mock-require'
 import sinon from 'sinon'
 const deconflict = sinon.stub()
-const filenamify = sinon.stub()
 let designate
 
 test.before(t => {
@@ -10,41 +9,34 @@ test.before(t => {
     '../../../../../lib/generate/separate/map/deconflict',
     deconflict
   )
-  mockRequire('filenamify', filenamify)
   designate = require('../../../../../lib/generate/separate/map/designate')
 })
 
 test.afterEach.always(t => {
   deconflict.reset()
-  filenamify.reset()
 })
 
 test.serial('basic', t => {
-  filenamify.returnsArg(0)
   deconflict.returnsArg(0)
   t.is(designate('apple', {}, {}), 'apple')
 })
 
 test.serial('suffix', t => {
-  filenamify.returnsArg(0)
   deconflict.returnsArg(0)
   t.is(designate('apple', {}, {}, '.js'), 'apple.js')
 })
 
 test.serial('encode', t => {
-  filenamify.returns('About!Company')
   deconflict.returnsArg(0)
-  t.is(designate('About/Company', {}, {}), 'About!Company')
+  t.is(designate('About/Company', {}, {}), 'AboutCompany')
 })
 
 test.serial('deconflict', t => {
-  filenamify.returnsArg(0)
   deconflict.returns('apple.A')
   t.is(designate('apple', {}, {}), 'apple.A')
 })
 
 test.serial('deconflict suffix', t => {
-  filenamify.returnsArg(0)
   deconflict.returns('apple.A')
   t.is(designate('apple', {}, {}, '.js'), 'apple.A.js')
 })
